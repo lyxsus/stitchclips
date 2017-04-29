1e344e9f2ea21010f6cd51670a1fbfa5259da7e0
Alexis Guerville <sadzeih@gmail.com>
2017-04-30 00:14:29 +0200

    Testing * Added GetTopClips tests

--- twitch_test.go
+++ twitch_test.go
@@ -12,13 +12,27 @@ var testClip = Clip{
 	},
 }
 
+func TestGetTopClips(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+	clips := Clips{}
+	clips.GetTopClips("itmejp", "2", "all")
+	if len(clips.Clips) != 2 {
+		t.Error("GetTopClips: did not get clips")
+	}
+	if clips.Clips[0].Broadcaster.Name != "itmejp" {
+		t.Error("GetTopClips: got the wrong clips")
+	}
+}
+
 func TestDownloadClip(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping test in short mode.")
 	}
 	testClip.DownloadClip("clips")
 	if _, err := os.Stat("clips/" + testClip.Slug + ".mp4"); os.IsNotExist(err) {
-		t.Error("File not downloaded")
+		t.Error("DownloadFile: File not downloaded")
 	}
 }
 
