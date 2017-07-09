ed6731aa91aff5af8b16902b6a4b4a6bde769ec6
Alexis Guerville <sadzeih@gmail.com>
2017-07-10 00:00:00 +0200

    feat: changes the way DownloadingManager keeps clips and downloads them one by one
    https://github.com/Sadzeih/stitchclips/issues/3

--- downloadingmanager.go
+++ downloadingmanager.go
@@ -6,82 +6,105 @@ import (
 )
 
 type DownloadingManager struct {
-	Outs        map[string]chan string
-	Errors      map[string]chan error
-	In          chan string
+	In         chan string
+	ClipsMutex sync.Mutex
+	Clips      map[string]DmClip
+}
+
+type DmClip struct {
+	Clip        Clip
 	outMutex    sync.Mutex
 	errorsMutex sync.Mutex
-	setMutex    sync.Mutex
-	Sets        map[string]Clips
+	Outs        map[string]chan string
+	Errs        map[string]chan error
+}
+
+func (dmClip DmClip) write(message string) {
+	for _, channel := range dmClip.Outs {
+		channel <- message
+	}
+}
+
+func (dmClip DmClip) writeError(err error) {
+	for _, channel := range dmClip.Errs {
+		channel <- err
+	}
 }
 
 func CreateDownloadingManager() DownloadingManager {
 	log.Println("Initializing DownloadingManager")
 	dm := DownloadingManager{
-		Outs:   make(map[string]chan string),
-		Errors: make(map[string]chan error),
-		In:     make(chan string),
-		Sets:   make(map[string]Clips),
+		In:    make(chan string, 8192),
+		Clips: make(map[string]DmClip),
 	}
 	return dm
 }
 
 func (this DownloadingManager) addClips(clips Clips) (chan string, chan error) {
 	// TODO: Check if the clips exists in the database before adding to the list; if it does exist, write on Out channel
-	this.outMutex.Lock()
-	this.Outs[clips.ID] = make(chan string)
-	this.outMutex.Unlock()
 
-	this.errorsMutex.Lock()
-	this.Errors[clips.ID] = make(chan error)
-	this.errorsMutex.Unlock()
+	outChan := make(chan string)
+	errChan := make(chan error)
 
-	this.setMutex.Lock()
-	if _, ok := this.Sets[clips.ID]; ok != true {
-		this.Sets[clips.ID] = clips
-		this.In <- clips.ID
+	for _, clip := range clips.Clips {
+		this.ClipsMutex.Lock()
+		if _, ok := this.Clips[clip.Slug]; ok != true {
+			this.Clips[clip.Slug] = DmClip{
+				Clip: clip,
+				Outs: make(map[string]chan string),
+				Errs: make(map[string]chan error),
+			}
+		}
+		this.Clips[clip.Slug].Outs[clips.ID] = outChan
+		this.Clips[clip.Slug].Errs[clips.ID] = errChan
+		this.ClipsMutex.Unlock()
+
+		this.In <- clip.Slug
+
 	}
-	this.setMutex.Unlock()
-	return this.Outs[clips.ID], this.Errors[clips.ID]
+	return outChan, errChan
+}
+
+func (this DownloadingManager) checkChannelAmount(id string) int {
+	amount := 0
+	for _, dmClip := range this.Clips  {
+		if _, ok := dmClip.Outs[id]; ok == true {
+			amount++
+		}
+	}
+	return amount
 }
 
 func (this DownloadingManager) run() {
 	log.Println("Starting Downloading Manager")
 	for true {
-		clipsID := <-this.In
-		clips := this.Sets[clipsID]
-		log.Println("[DownloadingManager] Getting and downloading ", clips.Slugs())
-		for _, clip := range clips.Clips {
-			err := clip.Get()
+		id := <-this.In
+		dmClip := this.Clips[id]
+		log.Println("[DownloadingManager] Getting and downloading ", dmClip.Clip.Slug)
+		err := dmClip.Clip.Get()
+		if err != nil {
+			dmClip.writeError(err)
+		} else {
+			err = dmClip.Clip.Download()
+			log.Println("[DownloadingManager] Finished: ", dmClip.Clip.Slug)
 			if err != nil {
-				this.Errors[clipsID] <- err
+				dmClip.writeError(err)
 			} else {
-				err = clip.Download()
-				log.Println("[DownloadingManager] Finished: ", clip.Slug)
-				if err != nil {
-					this.Errors[clipsID] <- err
-				} else {
-					// TODO: add the clip to the database
-					this.Outs[clipsID] <- clip.Slug
-					this.Errors[clipsID] <- nil
-				}
+				// TODO: add the clip to the database
+				dmClip.write(dmClip.Clip.Slug)
+				dmClip.writeError(nil)
 			}
 		}
-		close(this.Outs[clipsID])
 
-		close(this.Errors[clipsID])
+		if this.checkChannelAmount(id) == 1 {
+			close(dmClip.Outs[id])
+			close(dmClip.Errs[id])
+		}
 
-		this.setMutex.Lock()
-		delete(this.Sets, clipsID)
-		this.setMutex.Unlock()
+		this.ClipsMutex.Lock()
+		delete(this.Clips, dmClip.Clip.Slug)
+		this.ClipsMutex.Unlock()
 
-		this.outMutex.Lock()
-		delete(this.Outs, clipsID)
-		this.outMutex.Unlock()
-
-		this.errorsMutex.Lock()
-		delete(this.Errors, clipsID)
-		this.errorsMutex.Unlock()
-		log.Println("[DownladingManager] Done with: ", clips.Slugs())
+		log.Println("[DownladingManager] Done with: ", dmClip.Clip.Slug)
 	}
 }
