c4e272a58fc7f9039aee0b93ddd7afe823bdc374
Alexis Guerville <sadzeih@gmail.com>
2017-07-18 21:56:44 +0200

    style: rearange code files

--- clips_test.go
+++ clips_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"os"
 	"testing"
 )
 
@@ -27,13 +26,3 @@ func TestGetTop(t *testing.T) {
 	}
 }
 
-func TestDownload(t *testing.T) {
-	t.Log("Testing Download")
-	if testing.Short() {
-		t.Skip("skipping test in short mode.")
-	}
-	testClip.Download()
-	if _, err := os.Stat(a.Config.Path + "/" + testClip.Slug + ".mp4"); os.IsNotExist(err) {
-		t.Error("DownloadFile: File not downloaded")
-	}
-}
